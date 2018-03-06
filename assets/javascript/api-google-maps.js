fd4c3fe34d4d3e61fbd50a7995297167d831f825
Ben Garcia <ben@seebenprogram.com>
2018-03-06 15:48:51 -0500

    Add boilerplate for googleAPI object

--- assets/javascript/api-google-maps.js
+++ assets/javascript/api-google-maps.js
@@ -3,3 +3,12 @@ API DOC's:
 https://developers.google.com/maps/documentation/javascript/
 https://developers.google.com/maps/documentation/javascript/examples/place-search
 */
+
+const googleApi = {
+  apiKey: "AIzaSyB0YC_GpZvlge7TTOgdNhujspZ8yjdbvQU"
+  /* 
+   *  All google api properties/methods 
+   *  go inside this object 
+   * 
+   */
+};
