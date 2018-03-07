44a83a161e7cf97748daad8843d188c405b3d002
Ben Garcia <ben@seebenprogram.com>
2018-03-06 22:02:29 -0500

    Build rough draft of api call object to NHTSA

--- assets/javascript/api-nhtsa.js
+++ assets/javascript/api-nhtsa.js
@@ -1,46 +1,78 @@
 /*
-API DOC's:
-https://vpic.nhtsa.dot.gov/api/
+ * How to use this object:
+ *   - Access the array of available car 'makes' like this:
+ *       vehicleApi.makes;
+ *   - To initiate api call, call method like this, passing in the actual string of a make of vehicle: 
+ *       vehicleApi.searchModel(make);
+ *   - In either case, you will receive an array of makes or models in response
 */
 
 const vehicleApi = {
-  /* 
-   *  All NHTSA api properties/methods 
-   *  go inside this object 
-   */
-  // Possible Vehicle Makes
-  makes: [
-    "Acura","Alfa Romeo","Aston Martin","Audi","Bentley","BMW","Buick",
-    "Cadillac","Chevrolet","Chrysler","Dodge","Ferrari","FIAT","Ford",
-    "Freightliner","Genesis","GMC","Honda","Hyundai","INFINITI","Jaguar",
-    "Jeep","Kia","Lamborghini","Land Rover","Lexus","Lincoln","Lotus",
-    "Maserati","Mazda","McLaren","Mercedes-Benz","MINI","Mitsubishi",
-    "Nissan","Porsche","Ram","Rolls-Royce","smart","Subaru","Tesla",
-    "Toyota","Volkswagen","Volvo"
+  /*
+  API DOC's:
+  https://vpic.nhtsa.dot.gov/api/
+  */
+  // Available Vehicle Makes
+  _makes: [
+    "Acura","Audi","BMW","Buick","Cadillac","Chevrolet","Chrysler","Dodge",
+    "FIAT","Ford","Genesis","GMC","Honda","Hyundai","INFINITI","Jaguar","Jeep",
+    "Kia","Land Rover","Lexus","Lincoln","Lotus","Mazda","Mercedes-Benz","MINI",
+    "Mitsubishi","Nissan","Porsche","Ram","smart","Subaru","Tesla","Toyota",
+    "Volkswagen","Volvo"
   ],
   // Query Parameters
-  url: "https://vpic.nhtsa.dot.gov/api/vehicles",
-
+  _url: "https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/",
+  // Model year
+  _year: "/modelyear/",
+  // Will return 'Truck' vehicle type
+  _truck: "/vehicleType/truck?format=json",
+  // Will return 'Car' and 'SUV' vehicle type
+  _car: "/vehicleType/passenger?format=json",
+  // Return value of last api call
+  _modelObjArray: [],
+  get makes() {
+    return this._makes;
+  },
+  get models() {
+    let modelNameArray = this._modelObjArray.map(obj => obj.Model_Name);
+    console.log(`modelNameArray: ${modelNameArray}`);
+    return modelNameArray;
+  },
+  searchModel(year, make) {
+    // Ignore 'falsy' 'make' arg passed
+    if (make) {
+      // Convert year argument to string (in case passed as type number)
+      let stringYear = year.toString();
+      // Get query URL for cars and suvs
+      let carQueryUrl = this.createCarQueryUrl(year, make);
+      console.log(`carQueryUrl: ${carQueryUrl}`);
+      // Get query URL for trucks
+      let truckQueryUrl = this.createTruckQueryUrl(year, make);
+      console.log(`truckQueryUrl: ${truckQueryUrl}`);
+      // Call API with both query URL's
+      let carObjArray = this.callAPI(carQueryUrl, truckQueryUrl);
+      console.log(`carObjArray: ${carObjArray}`);
+      return carObjArray;
+    }
+    console.log(`vehicleApi.searchModel() 'make' falsy value passed: ${make}`);
+    return make;
+  },
+  createCarQueryUrl(year, make) {
+    return this._url + make + this._year + year + this._truck;
+  },
+  createTruckQueryUrl(year, make) {
+    return this._url + make + this._year + year + this._car;
+  },
+  callAPI(carsUrl, trucksUrl) {
+    let carObjArray;
+    // 2 async calls (one for car/suv models, one for trucks -- API limitation)
+    $.when( $.ajax(carsUrl), $.ajax(trucksUrl) ).then( (a1, a2) => {
+      // If success, combine 2 response object 'Results' arrays
+      carObjArray = a1[0].Results.concat(a2.[0].Results);
+      return carObjArray;
+    }, (err) => {
+      console.log(`vehicleApi call error thrown: ${err}`);
+    }
+    );
+  }
 };
-
-/*
-1) User selects Year
-
-2) User selects Make - (Hard coded list of car 'makes')
-   - causes propagation of available models from NHTSA API
-
-   "Passenger%20Car"
-   "Truck"
-
-   Problem: Take the year and the make, return all possible models (exclude motorcycle, trailers, etc.)
-
-   Sample query url to use to find all possible vehicle models available for a make:
-   https://vpic.nhtsa.dot.gov/api/vehicles
-    
-3) User selects Model
-
-4) User inputs mileage
-
-5) Form is submitted
-
-*/
