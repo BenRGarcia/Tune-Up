5876617a50b93d2db5f40b60b0890ff14fe50550
Ben Garcia <ben@seebenprogram.com>
2018-03-08 11:00:31 -0500

    Hide ugly square while login submission pending

--- assets/javascript/firebase-user.js
+++ assets/javascript/firebase-user.js
@@ -3,26 +3,10 @@ initApp = function() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in
-      console.log(`User is signed in`);
-      // Get user info
-      user.getIdToken().then(function(accessToken) {
-        document.getElementById('account-details').textContent = JSON.stringify({
-          // email:         user.email,
-          // emailVerified: user.emailVerified,
-          // phoneNumber:   user.phoneNumber,
-          // photoURL:      user.photoURL,
-          // accessToken:   accessToken,
-          // providerData:  user.providerData
-          displayName:   user.displayName,
-          uid:           user.uid
-        }, null, '  ');
-      });
+      console.log(`Logged in user: displayName = '${user.displayName}', uid = '${user.uid}'`);
     } else {
       // user is signed out
       console.log(`User is signed out`);
-      document.getElementById('sign-in-status').textContent = 'Signed out';
-      document.getElementById('sign-in').textContent = 'Sign in';
-      document.getElementById('account-details').textContent = 'null';
     }
   }, function(error) {
     console.log(error);
@@ -35,41 +19,53 @@ window.addEventListener('load', function() {
 
 // Object to return 'uid' for use with firebase db
 const userData = {
+  _name: null,
+  _uid: null,
+  get getUid() {
+    return this._uid
+  },
+  get getName() {
+    return this._name
+  },
   retrieveUid() {
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
-        return user.uid
+        return this._uid = user.uid;
       } else {
         console.log(`firebase-user.js says: "Cannot retrieve uid, user not logged in"`);
       }
+    }, function(error) {
+      console.log(error);
     });
   },
   retrieveName() {
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
-        return user.displayName
+        return this._name = user.displayName;
       } else {
         console.log(`firebase-user.js says: "Cannot retrieve displayName, user not logged in"`);
       }
+    }, function(error) {
+      console.log(error);
     });
   }
 };
 
 /* // Sample data available for firebase auth ui user:
 { 
-  "displayName": "Ben Garcia", 
-  "email": "ben@seebenprogram.com", 
+  "displayName": "Ben...", 
+  "email": "ben@email.com", 
   "emailVerified": false, 
   "phoneNumber": null, 
-  "photoURL": "https://avatars3.githubusercontent.com/u/26657982?v=4", 
-  "uid": "I9QtY5OfJLdgqLuzD6QWBcKHcCG2", 
-  "accessToken": "eyJhbGciOiJSU...", 
+  "photoURL": "https://avatars3.gith...", 
+  "uid": "I9QtY5OfJLdgqLuz...", 
+  "accessToken": "eyJh.....................", 
   "providerData": [ 
     { 
-      "uid": "26657982", 
-      "displayName": "Ben Garcia", 
-      "photoURL": "https://avatars3.githubusercontent.com/u/26657982?v=4", 
-      "email": "ben@seebenprogram.com", 
+      "uid": "26......", 
+      "displayName": "Ben...", 
+      "photoURL": "https://avatars3.gith...", 
+      "email": "ben@email.com", 
       "phoneNumber": null, 
       "providerId": "github.com" 
     } 
