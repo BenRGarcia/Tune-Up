d10548c04705394af2ccbe15f9e746c415304226
Ben Garcia <ben@seebenprogram.com>
2018-03-08 08:16:15 -0500

    Build object/method to retrieve uid from firebase auth

--- assets/javascript/firebase-user.js
+++ assets/javascript/firebase-user.js
@@ -7,14 +7,14 @@ initApp = function() {
       // Get user info
       user.getIdToken().then(function(accessToken) {
         document.getElementById('account-details').textContent = JSON.stringify({
+          // email:         user.email,
+          // emailVerified: user.emailVerified,
+          // phoneNumber:   user.phoneNumber,
+          // photoURL:      user.photoURL,
+          // accessToken:   accessToken,
+          // providerData:  user.providerData
           displayName:   user.displayName,
-          email:         user.email,
-          emailVerified: user.emailVerified,
-          phoneNumber:   user.phoneNumber,
-          photoURL:      user.photoURL,
-          uid:           user.uid,
-          accessToken:   accessToken,
-          providerData:  user.providerData
+          uid:           user.uid
         }, null, '  ');
       });
     } else {
@@ -32,3 +32,47 @@ initApp = function() {
 window.addEventListener('load', function() {
   initApp()
 });
+
+// Object to return 'uid' for use with firebase db
+const userData = {
+  retrieveUid() {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        return user.uid
+      } else {
+        console.log(`firebase-user.js says: "Cannot retrieve uid, user not logged in"`);
+      }
+    });
+  },
+  retrieveName() {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        return user.displayName
+      } else {
+        console.log(`firebase-user.js says: "Cannot retrieve displayName, user not logged in"`);
+      }
+    });
+  }
+};
+
+/* // Sample data available for firebase auth ui user:
+{ 
+  "displayName": "Ben Garcia", 
+  "email": "ben@seebenprogram.com", 
+  "emailVerified": false, 
+  "phoneNumber": null, 
+  "photoURL": "https://avatars3.githubusercontent.com/u/26657982?v=4", 
+  "uid": "I9QtY5OfJLdgqLuzD6QWBcKHcCG2", 
+  "accessToken": "eyJhbGciOiJSU...", 
+  "providerData": [ 
+    { 
+      "uid": "26657982", 
+      "displayName": "Ben Garcia", 
+      "photoURL": "https://avatars3.githubusercontent.com/u/26657982?v=4", 
+      "email": "ben@seebenprogram.com", 
+      "phoneNumber": null, 
+      "providerId": "github.com" 
+    } 
+  ] 
+}
+*/
