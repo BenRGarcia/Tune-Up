f21db526322bb2091f4be0395c4fe203bdd4c380
Ben Garcia <ben@seebenprogram.com>
2018-03-10 12:30:56 -0500

    Build object shell to hold all DOM render methods

---
+++ assets/javascript/render.js
@@ -0,0 +1,14 @@
+const DOM = {
+  // render 'cars' to the browser
+  renderCars() {
+    
+  },
+  // render 'lastMaintenance'
+  renderLastMaintenance() {
+    
+  },
+  // render 'maintenanceIntervals' to input placeholders
+  renderMaintenanceIntervals() {
+    
+  }
+};
