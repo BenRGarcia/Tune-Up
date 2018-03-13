a30e1981e648cfd74ea1616e12d934ca00aef7d1
Your Name <melissarburnham@gmail.com>
2018-03-13 16:20:27 -0400

    calculations for next service

--- assets/javascript/update-timeline.js
+++ assets/javascript/update-timeline.js
@@ -19,6 +19,9 @@ const maintenanceTimeline = {
         let inspectionInterval = carObject.maintenanceInterval.carInspectionMonths;
         let wiperInterval = carObject.maintenanceInterval.wiperBladesMonths;
 
+        let currentMileage =carObject.mileage;
+        
+
 
  /*
      *  Compute Date Math Stuff, Add properties to object if truthy
@@ -26,15 +29,27 @@ const maintenanceTimeline = {
     // If not falsey
     if (oilLast) {
         // Compute Miles Oil Change
-        let nextOil = ?; // should store something like "2018/03/28"
+        let diff = currentMileage - oilLast;
+        let oilDiff = oilInterval - diff;
+        let oilDivide = oilDiff / this.monthMiles;
+        let oilMath = moment().add(oilDivide, 'M');
+        let oilDefine = oilMath._d;
+        let nextOil = moment(oilMath).format("MM/DD/YYYY");
+        console.log(nextOil, "NEXT OIL");
         // Add property to maintenance object
         maintenanceObject.oil = nextOil;
       }
   
-      // If not falsey
+    //   // If not falsey
       if (tireLast) {
         // Compute Miles Tire Change
-        let nextTire = ?; // should store something like "2018/03/28"
+        let diff = currentMileage - tireLast;
+        let tireDiff = tireInterval - diff;
+        let tireDivide = tireDiff / this.monthMiles;
+        let tireMath = moment().add(tireDivide, 'M');
+        let tireDefine = tireMath._d;
+        let nextTire = moment(tireMath).format("MM/DD/YYYY");
+        console.log(nextTire, "NEXT TIRE");
         // Add property to maintenance object
         maintenanceObject.tire = nextTire;
       }
@@ -42,7 +57,11 @@ const maintenanceTimeline = {
       // If not falsey
       if (brakeLast) {
         // Compute Months Brake Change
-        let nextBrake = ?; // should store something like "2018/03/28"
+        let brakeLastUnix = dateConverter.unixTimeTommddyyyy(brakeLast);
+        let nextBrakeMath = moment(brakeLastUnix).add(brakeInterval, 'M');
+        let nextBrakeDefine = nextBrakeMath._d;
+        let nextBrake = moment(nextBrakeDefine).format("MM/DD/YYYY");
+        console.log(nextBrake, "NEXT BRAKE");
         // Add property to maintenance object
         maintenanceObject.brake = nextBrake;
       }
@@ -50,7 +69,11 @@ const maintenanceTimeline = {
       // If not falsey
       if (inspectionLast) {
         // Compute Months Inspection Change
-        let nextInspection = ?; // should store something like "2018/03/28"
+        let inspectionLastUnix = dateConverter.unixTimeTommddyyyy(inspectionLast);
+        let nextInspectionMath = moment(inspectionLastUnix).add(inspectionInterval, 'M');
+        let nextInspectionDefine = nextInspectionMath._d;
+        let nextInspection = moment(nextInspectionDefine).format("MM/DD/YYYY");
+        console.log(nextInspection, "NEXT INSPECTION");
         // Add property to maintenance object
         maintenanceObject.inspection = nextInspection;
       }
@@ -58,7 +81,11 @@ const maintenanceTimeline = {
       // If not falsey
       if (wiperLast) {
         // Compute Months Wiper Change
-        let nextWiper = ?; // should store something like "2018/03/28"
+        let wiperLastUnix = dateConverter.unixTimeTommddyyyy(wiperLast);
+        let nextWiperMath = moment(wiperLastUnix).add(wiperInterval, 'M');
+        let nextWiperDefine = nextWiperMath._d;
+        let nextWiper = moment(nextWiperDefine).format("MM/DD/YYYY");
+        console.log(nextWiper, "NEXT WIPER");
         // Add property to maintenance object
         maintenanceObject.wiper = nextWiper;
       }
