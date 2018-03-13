c619d46776c50fa88bbc339e9b83cc300e26dcb9
Ben Garcia <ben@seebenprogram.com>
2018-03-13 12:02:12 -0400

    Temp change to localhost sign in redirect

--- assets/javascript/firebase-auth.js
+++ assets/javascript/firebase-auth.js
@@ -5,7 +5,7 @@
 
 // FirebaseUI config.
 var uiConfig = {
-  signInSuccessUrl: 'https://benrgarcia.github.io/Tune-Up/virtual-garage.html',
+  signInSuccessUrl: 'http://localhost:8888/virtual-garage.html',
   signInOptions: [
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID
