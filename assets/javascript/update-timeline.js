af2cf4d86d1ca21f773ae918068249a49eb05e97
Your Name <melissarburnham@gmail.com>
2018-03-13 07:43:29 -0400

    create js to update timeline

--- assets/javascript/update-timeline.js
+++ assets/javascript/update-timeline.js
@@ -2,6 +2,7 @@ const updateTimeline = {
 
     render: function(object){
 
+    
      let inspectionDate = $('#js-display-last-car-inspection').val();
      let brakeDate = $('#js-display-last-brake-inspection').val();
      let wiperBladeDate = $('#js-display-last-wiper-blades').val();
@@ -18,12 +19,96 @@ const updateTimeline = {
     //  let brakeInpectionUnixTime = dateConverter.unixTimeTommddyyyy(brakeDate);
     //  let wiperBladesUnixTime = dateConverter.unixTimeTommddyyyy(wiperBladeDate);    
 
-     let carInspectionTimeline = addCarMonths.data("date");
-     let brakeInspectionTimeline = addBrakeMonths.data("date");
-     let wiperBladeTimeline = addWiperMonths.data("date");
-     let oilChangeTimeline = addOilMonths.data("date");
-     let tireRotationTimeline = addTireMonths.data("date");
+     let carInspectionTimeline = addCarMonths.attr("data-date",addCarMonths);
+     let brakeInspectionTimeline = addBrakeMonths.attr("data-date",addBrakeMonths);
+     let wiperBladeTimeline = addWiperMonths.attr("data-date",addWiperMonths);
+     let oilChangeTimeline = addOilMonths.attr("data-date",addOilMonths);
+     let tireRotationTimeline = addTireMonths.attr("data-date",addTireMonths);
+     
+    //  var list = "timelineDates"
 
+    //  if(!Array.isArray(list)){
+    //         list= [];
+    //     }
+    
+    $("#timelineDates").empty();
+    $("#timelineDisplay").empty(); 
+
+    if(carInspectionTimeline){
+        var tDateCarInspection = ('<li><a href="#0">'+ carInspectionTimeline +'</a></li>');
+        
+        var tDateCarInspectionDisplay = ('<li>');
+        tDateCarInspectionDisplay.attr(carInspectionTimeline);
+        tDateCarInspectionDisplay.attr("id", "js-timeline-title-car-inspection");
+        tDateCarInspection.append('<h2>' + 'CAR INSPECTION' + '</h2>');
+
+        $("#timelineDates").append(tDateCarInspection);
+        $("#timelineDisplay").append(tDateCarInspectionDisplay);
+
+    };
+
+    if(brakeInspectionTimeline){
+        var tDateBrakeInspection = ('<li><a href="#0">'+ brakeInspectionTimeline +'</a></li>');
+        
+        var tDateBrakeInspectionDisplay = ('<li>');
+        tDateBrakeInspectionDisplay.attr(brakeInspectionTimeline);
+        tDateBrakeInspectionDisplay.attr("id", "js-timeline-title-brake-inspection");
+        tDateBrakeInspection.append('<h2>' + 'BRAKE INSPECTION' + '</h2>');
+
+        $("#timelineDates").append(tDateBrakeInspection);
+        $("#timelineDisplay").append(tDateBrakeInspectionDisplay);
+
+    };
+
+    if(wiperBladeTimeline){
+        var tDateWiperBlade = ('<li><a href="#0">'+ wiperBladeTimeline +'</a></li>');
+        
+        var tDateWiperBladeDisplay = ('<li>');
+        tDateWiperBladeDisplay.attr(wiperBladeTimeline);
+        tDateWiperBladeDisplay.attr("id", "js-timeline-title-wiper-blades");
+        tDateWiperBlade.append('<h2>' + 'CHECK WIPER BLADES' + '</h2>');
+
+        $("#timelineDates").append(tDateWiperBlade);
+        $("#timelineDisplay").append(tDateWiperBladeDisplay);
+
+    };
+
+    if(oilChangeTimeline){
+        var tDateOilChange = ('<li><a href="#0">'+ oilChangeTimeline +'</a></li>');
+        
+        var tDateOilChangeDisplay = ('<li>');
+        tDateOilChangeDisplay.attr(oilChangeTimeline);
+        tDateOilChangeDisplay.attr("id", "js-timeline-title-oil-change");
+        tDateOilChange.append('<h2>' + 'CHANGE OIL' + '</h2>');
+
+        $("#timelineDates").append(tDateOilChange);
+        $("#timelineDisplay").append(tDateOilChangeDisplay);
+    };
+
+    if(tireRotationTimeline){
+        var tDateTireRotation = ('<li><a href="#0">'+ tireRotationTimeline +'</a></li>');
+        
+        var tDateTireRotationDisplay = ('<li>');
+        tDateTireRotationDisplay.attr(tireRotationTimeline);
+        tDateTireRotationDisplay.attr("id", "js-timeline-title-tire-change");
+        tDateTireRotation.append('<h2>' + 'TIRE ROTATION' + '</h2>');
+
+        $("#timelineDates").append(tDateTireRotation);
+        $("#timelineDisplay").append(tDateTireRotationDisplay);
+    };
+
+    
+
+         // var insideList = "timelineDates"   
+
+        //  if(!Array.isArray(insideList)){
+        //      insideList = [];
+        //  }
+
+        //  for(var i = 0; i < insideList.length; i++){
+        //     var tDates = ("<li>").
+
+     
       // Declare variables
     //   let lastOilChange       = object.oilChange;
     //   let lastTireRotation    = object.tireRotation;
@@ -32,11 +117,11 @@ const updateTimeline = {
     //   let lastWiperBlades     = object.wiperBladesUnixTime;
 
       // Update DOM
-      $('#timelineDates').append(carInspectionTimeline);
-      $('#timelineDates').append(brakeInspectionTimeline);
-      $('#timelineDates').append(wiperBladeTimeline);
-      $('#timelineDates').append(oilChangeTimeline);
-      $('#timelineDates').append(tireRotationTimeline);
+    //   $('#timelineDates').append(carInspectionTimeline);
+    //   $('#timelineDates').append(brakeInspectionTimeline);
+    //   $('#timelineDates').append(wiperBladeTimeline);
+    //   $('#timelineDates').append(oilChangeTimeline);
+    //   $('#timelineDates').append(tireRotationTimeline);
 
         //CREATE ARRAY, 
         //PUSH DATA TO ARRAY, 
